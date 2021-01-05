4797cad3ef03e03d54e7bbdd396a0b28fae8de32
za5dwunasta <magdalena.motyl@gmail.com>
2021-01-05 16:07:59 +0000

    Board created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,58 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+
+enum StatusType {
+	cover,
+	uncover,
+	flagged,
+}
+
+enum ValueType {
+	zero,
+	one,
+	two,
+	three,
+	four,
+	five,
+	six,
+	seven,
+	eight,
+	bomb,
+}
+
+type Cell = {
+	status: StatusType;
+	value: ValueType;
+};
 
 function App() {
-  return (
-    <div className="App">
-  Test
- uu
-    </div>
-  );
+	const [cells, setCells] = useState<Cell[]>([]);
+
+	useEffect(() => {
+		let _cells: Cell[] = [];
+		_cells.length = 81;
+		for (let i = 0; i < 81; i++) {
+			_cells[i] = { status: StatusType.cover, value: ValueType.bomb };
+		}
+		setCells(_cells);
+		console.log(cells);
+	}, []);
+
+	const handleClick = (index: number) => {
+		let _cells: Cell[] = [...cells];
+		_cells[index].status = StatusType.uncover;
+		setCells(_cells);
+	};
+
+	return (
+		<div className="App">
+			<div className="board">
+				{cells.map((item, index) => (
+					<button onClick={() => handleClick(index)} className="board__cell" key={index}></button>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
